42f7a821f368604a228c7496932370f969676ef7
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-05-11 16:46:56 +0530

    added the override method in quantity measurement

--- src/main/java/com/measurement/QuantityMeasurement.java
+++ src/main/java/com/measurement/QuantityMeasurement.java
@@ -4,4 +4,15 @@ public class QuantityMeasurement {
     public double returnLengthType(LengthType type, double unit) {
         return unit * type.conversionUnit;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (this.getClass() == obj.getClass())
+            return true;
+        if (obj == this)
+            return true;
+        return super.equals(obj);
+    }
 }
